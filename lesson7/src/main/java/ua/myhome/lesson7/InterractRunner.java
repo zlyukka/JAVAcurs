20b8f47d39b0cf4847c841b636970b677b8e1902
zlyukka <vitaliy_kov@mail.ru>
2016-07-26 00:07:06 +0300

    Урок7 наследование

---
+++ lesson7/src/main/java/ua/myhome/lesson7/InterractRunner.java
@@ -0,0 +1,29 @@
+package ua.myhome.lesson7;
+
+import java.util.Scanner;
+class InterractRunner{
+    public static void main(String[] arg){
+			Scanner scan=new Scanner(System.in);
+			System.out.println("Start calc model 2");
+			try{
+				final Calculator calc=new Calculator();
+				String exit="no";
+				while(!exit.equals("yes")){
+					System.out.println("Enter par 1");
+					String first=scan.next();
+					System.out.println("Enter par 2");
+					String second=scan.next();
+					calc.startCalculation(Double.valueOf(first), Double.valueOf(second));
+					System.out.println("result sum="+calc.getResultSun());
+					System.out.println("result minus="+calc.getResultMin());
+					System.out.println("result mnog="+calc.getResultMnog());
+					System.out.println("result del="+calc.getResultDel());
+					System.out.println("EXIT yes/no");
+					exit=scan.next();
+				}
+			}finally {
+				scan.close();
+			}
+
+	}
+}
